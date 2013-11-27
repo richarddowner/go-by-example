3a30648d7be3c7bc8dc6b9be6580cdf01280c6fd
Richard Downer <richarddowna@gmail.com>
2013-11-28 08:58:03 +1300

    slices comments

--- slices.go
+++ slices.go
@@ -5,36 +5,36 @@ import "fmt"
 func main() {
 
 	s := make([]string, 3)
-	fmt.Println("emp:", s)
+	fmt.Println("emp:", s) // emp: [ ]
 
 	s[0] = "a"
 	s[1] = "b"
 	s[2] = "c"
 
-	fmt.Println("set:", s)
-	fmt.Println("get:", s[2])
+	fmt.Println("set:", s) // set: [a b c]
+	fmt.Println("get:", s[2]) // get: c
 
-	fmt.Println("len:", len(s))
+	fmt.Println("len:", len(s)) // len: 3
 
 	s = append(s, "d")
 	s = append(s, "e", "f")
-	fmt.Println("apd:", s)
+	fmt.Println("apd:", s) // apd: [a b c d e f]
 
 	c := make([]string, len(s))
-	copy(c,s)
-	fmt.Println("cpy:", c)
+	copy(c,s) // copy into c the contents of s
+	fmt.Println("cpy:", c) // cpy: [a b c d e f]
 
-	l := s[2:5]
-	fmt.Println("sl1:", l)
+	l := s[2:5] // [inclusive:non-inclusive]
+	fmt.Println("sl1:", l) // sl1: [c d e]
 
 	l = s[:5]
-	fmt.Println("sl2:", l)
+	fmt.Println("sl2:", l) // sl2: [a b c d e]
 
 	l = s[2:]
-	fmt.Println("sl3:", l)
+	fmt.Println("sl3:", l) // sl3: [c d e f]
 
 	t := []string{"g","h","i"}
-	fmt.Println("dcl:", t)
+	fmt.Println("dcl:", t) // dcl: [g h i]
 
 	twoD := make([][]int, 3)
 	for i := 0; i < 3; i++ {
@@ -44,5 +44,5 @@ func main() {
 			twoD[i][j] = i + j
 		}
 	}
-	fmt.Println("2d: ", twoD)
+	fmt.Println("2d: ", twoD) // 2d: [[0] [1 2] [1 2 3]]
 }
