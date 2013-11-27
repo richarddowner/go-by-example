ead0db93ce4ba54d3244e088cb4bc5f16573ef96
Richard Downer <richarddowna@gmail.com>
2013-11-28 09:34:05 +1300

    arrays comments

--- arrays.go
+++ arrays.go
@@ -5,16 +5,16 @@ import "fmt"
 func main() {
 
 	var a [5]int
-	fmt.Println("emp:", a)
+	fmt.Println("emp:", a) // emp: [0 0 0 0 0]
 
 	a[4] = 100
-	fmt.Println("set:", a)
-	fmt.Println("get:", a[4])
+	fmt.Println("set:", a) // set: [0 0 0 0 100]
+	fmt.Println("get:", a[4]) // get: 100
 
-	fmt.Println("len:", len(a))
+	fmt.Println("len:", len(a)) // len: 5
 
 	b := [5]int{1,2,3,4,5}
-	fmt.Println("dcl:", b)
+	fmt.Println("dcl:", b) // dcl: [1 2 3 4 5]
 
 	var twoD [2][3]int
 	for i:= 0; i < 2; i++ {
@@ -22,5 +22,5 @@ func main() {
 			twoD[i][j] = i + j
 		}
 	}
-	fmt.Println("2d: ", twoD)
+	fmt.Println("2d: ", twoD) // 2d: [[0 1 2] [1 2 3]]
 }
