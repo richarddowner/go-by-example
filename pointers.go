6b424d34dd36aaf257dc0f12bd0422bb4b617150
Richard Downer <richard.downer@xero.com>
2013-12-02 08:27:08 +1300

    Pointers

---
+++ pointers.go
@@ -0,0 +1,29 @@
+/*
+When a pointer is created within a local scope,
+you create memory on the stack that points to
+a space on the heap.
+*/
+package main
+
+import "fmt"
+
+func zeroval(ival int) {
+	ival = 0
+}
+
+func zeroptr(iptr *int) {
+	*iptr = 0
+}
+
+func main() {
+	i := 1
+	fmt.Println("initial:", i) // initial: 1
+
+	zeroval(i)
+	fmt.Println("zeroval:", i) // zeroval: 1
+
+	zeroptr(&i)
+	fmt.Println("zeroptr:", i) // zeroptr: 0
+
+	fmt.Println("pointer:", &i) // pointer: 0x0000000
+}
