493292e2695cd850d6ff506221b8ba19604d53cb
Richard Downer <richard.downer@xero.com>
2013-12-04 08:38:42 +1300

    channels comments

--- channels.go
+++ channels.go
@@ -3,6 +3,12 @@ Channels are the pipes that connect concurrent
 goroutines. You can send values into channels
 from one goroutine and receive those values into
 another goroutine.
+
+By default sends and receives block until both
+the sender and receiver are ready. This property
+allowed us to wait at the end of our program for
+the 'ping' message without having to use any other
+synchronization.
 */
 package main
 
