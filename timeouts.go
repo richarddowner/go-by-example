480f7f7b3521185698b017efc4000155f24d92d1
Richard Downer <richard.downer@xero.com>
2013-12-06 08:39:13 +1300

    timeouts comments

--- timeouts.go
+++ timeouts.go
@@ -1,3 +1,11 @@
+/*
+Important for programs that connect to
+external resources or that otherwise need
+to bound execution time.
+
+Implementing timeouts in Go is easy and
+elegant thanks to channels and select.
+*/
 package main
 
 import (
@@ -7,12 +15,17 @@ import (
 
 func main() {
 
+	// exec an external call that returns
+	// its result on a channel c1 after 2s.
 	c1 := make(chan string, 1)
 	go func() {
 		time.Sleep(time.Second * 2)
 		c1 <- "result 1"
 	}()
 
+	// res and <-time awaits the result
+	// since res takes 2s, <-time returns first
+	// timeout
 	select {
 	case res := <-c1:
 		fmt.Println(res)
@@ -26,6 +39,9 @@ func main() {
 		c2 <- "result 2"
 	}()
 
+	// res and <-time awaits the result
+	// since res takes 2s, res returns first
+	// no timeout
 	select {
 	case res := <-c2:
 		fmt.Println(res)
