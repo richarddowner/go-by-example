3cc983a81aeb2845d9fa8b8d271dc1750e38ad5e
Richard Downer <richard.downer@xero.com>
2013-12-10 08:31:25 +1300

    non blocking channel operations comments

--- non-blocking-channel-operations.go
+++ non-blocking-channel-operations.go
@@ -1,3 +1,9 @@
+/*
+Basic sends and receives on channels are blocking.
+However, we can use select with a default clause to
+implement non-blocking sends, receives, and even
+non-blocking multi-way selects.
+*/
 package main
 
 import (
